c0ffe0ea77c7b65eefb9b5a82216abacc9f6871b
Gabriel Silva <gabrielcanto461@gmail.com>
2023-07-18 23:33:41 -0300

    FEATURE: Adds main feature to the main page

--- src/app/page.js
+++ src/app/page.js
@@ -1,113 +1,163 @@
-import Image from 'next/image'
+'use client'
+import {Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle} from "@/components/ui/card";
+import {Table, TableBody, TableCaption, TableCell, TableHead, TableHeader, TableRow} from "@/components/ui/table";
+import {useEffect, useState} from "react";
+import {Button} from "@/components/ui/button";
+import {ScrollArea} from "@/components/ui/scroll-area";
+import {Popover, PopoverContent, PopoverTrigger} from "@/components/ui/popover";
+import {Label} from "@/components/ui/label";
+import {Input} from "@/components/ui/input";
+import axios from "axios";
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    const [ticker, setTicker] = useState('EXPL44');
+    const [name, setName] = useState('Empresa exemplo');
+    const [currentPrice, setCurrentPrice] = useState('R$ 10.00');
+    const [quantity, setQuantity] = useState('2');
+
+    const getCurrentDate = () => {
+        const monthNames = ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho",
+            "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"
+        ];
+
+        let current_datetime = new Date();
+        let formatted_date = current_datetime.getDate() + " " + monthNames[current_datetime.getMonth()] + " " + current_datetime.getFullYear();
+        return formatted_date;
+    }
+
+    const [tickets, setTickets] = useState([]);
+    const handleAddTicker = () => {
+        setTickets(tickets => [...tickets, {ticker, name, currentPrice, quantity}]);
+    };
+
+    const [availableTickers, setAvailableTickers] = useState([]);
+
+    useEffect(() => {
+        const searchForAvailableTickers = async () => {
+            try {
+                const res = await axios.get('https://brapi.dev/api/available');
+                setAvailableTickers(res.data.stocks);
+            } catch (error) {
+                console.error("Erro ao buscar os dados:", error);
+            }
+        };
+        searchForAvailableTickers();
+    }, []);
+
+    const [suggestions, setSuggestions] = useState([]);
+
+    useEffect(() => {
+        setSuggestions(availableTickers.filter(ticket => ticket.includes(ticker)));
+    }, [ticker]);
+
+    const handleTickerChange = (e) => {
+        const value = e.target.value;
+
+        if (availableTickers.includes(value)) {
+            setTicker(value);
+        }
+    };
+
+
+    console.log(availableTickers)
+    return (
+        <div className="flex min-h-screen bg-slate-50 items-center flex-col gap-2">
+            <Card className="w-[800px] h-[150px]">
+                <CardHeader>
+                    <CardTitle>Saldo Atual:</CardTitle>
+                    <CardDescription>{getCurrentDate()}</CardDescription>
+                </CardHeader>
+                <CardContent>
+                    <h1>R$ 15800,00</h1>
+                </CardContent>
+            </Card>
+            <Card className="w-[800px] h-[100%]">
+                <CardHeader>
+                    <CardTitle>Principais ativos:</CardTitle>
+                </CardHeader>
+                <CardContent>
+                    <ScrollArea className="h-[55vh] w-[100%] rounded-md border p-4">
+                        <Table>
+                            <TableHeader>
+                                <TableRow>
+                                    <TableHead className="w-[100px]">Ticker</TableHead>
+                                    <TableHead>Nome</TableHead>
+                                    <TableHead>Preço Atual</TableHead>
+                                    <TableHead>Quantidade</TableHead>
+                                    <TableHead>Posição</TableHead>
+                                </TableRow>
+                            </TableHeader>
+                            <TableBody>
+                                {tickets.map((ticket, index) => (
+                                    <TableRow key={index}>
+                                        <TableCell className="font-medium">{ticket.ticker}</TableCell>
+                                        <TableCell>{ticket.name}</TableCell>
+                                        <TableCell>{ticket.currentPrice}</TableCell>
+                                        <TableCell className="text-green-700">{ticket.quantity}</TableCell>
+                                        <TableCell>R$ {(ticket.currentPrice.substring(3)*ticket.quantity).toFixed(2)}</TableCell>
+                                    </TableRow>
+                                ))}
+                            </TableBody>
+                        </Table>
+                    </ScrollArea>
+                </CardContent>
+                <CardFooter className="flex justify-end">
+                    <Popover>
+                        <PopoverTrigger asChild>
+                            <Button variant="outline">Open popover</Button>
+                        </PopoverTrigger>
+                        <PopoverContent className="w-500">
+                            <div className="grid gap-4">
+                                <div className="grid gap-3">
+                                    <div className="grid grid-cols-3 items-center gap-4">
+                                        <Label htmlFor="ticker">Ticker</Label>
+                                        <Input
+                                            id="ticker"
+                                            defaultValue={ticker}
+                                            onChange={handleTickerChange}
+                                            className="col-span-2 h-8"
+                                        />
+                                        <datalist id="tickers-sugestoes">
+                                            {suggestions.map((suggestion, index) => (
+                                                <option key={index} value={suggestion} />
+                                            ))}
+                                        </datalist>
+                                    </div>
+                                    <div className="grid grid-cols-3 items-center gap-4">
+                                        <Label htmlFor="name">Nome</Label>
+                                        <Input
+                                            id="name"
+                                            defaultValue={name}
+                                            onChange={e => setName(e.target.value)}
+                                            className="col-span-2 h-8"
+                                        />
+                                    </div>
+                                    <div className="grid grid-cols-3 items-center gap-4">
+                                        <Label htmlFor="price">Preço Atual </Label>
+                                        <Input
+                                            id="price"
+                                            defaultValue={currentPrice}
+                                            onChange={e => setCurrentPrice(e.target.value)}
+                                            className="col-span-2 h-8"
+                                        />
+                                    </div>
+                                    <div className="grid grid-cols-3 items-center gap-4">
+                                        <Label htmlFor="quantity">Quantidade</Label>
+                                        <Input
+                                            id="quantity"
+                                            defaultValue={quantity}
+                                            onChange={e => setQuantity(e.target.value)}
+                                            className="col-span-2 h-8"
+                                        />
+                                    </div>
+                                    <Button onClick={() => handleAddTicker()} variant='default' size='default'
+                                            className="items-center gap-4">Confirmar</Button>
+                                </div>
+                            </div>
+                        </PopoverContent>
+                    </Popover>
+                </CardFooter>
+            </Card>
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+    )
 }
